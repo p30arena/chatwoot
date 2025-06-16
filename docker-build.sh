004ccac117fb31c27d68fad1a2e51fc3cbdd3ea2
aliep <p30arena@gmail.com>
2025-06-16 23:21:55 +0300

    remove typo

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
-docker compose build baseAdd commentMore actions
+docker compose build base
 docker compose build
 docker compose run --rm rails bundle exec rails db:chatwoot_prepare
 docker-compose run --rm rails bundle exec "BUILD_MODE=library bin/vite build"
